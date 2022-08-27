22c3f2e5d9c4f5834c6284f4eefad2683fe9483e
nitslaszlo <nitslaszlo@gmail.com>
2022-08-27 20:04:02 +0200

    up

--- src/store/axios.instance.ts
+++ src/store/axios.instance.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// const $axios = axios.create({
+//   baseURL: "https://epitmenyado-backend.herokuapp.com/",
+//   withCredentials: true,
+// });
+
 const $axios = axios.create({
-  baseURL: "https://epitmenyado-backend.herokuapp.com/",
+  baseURL: "https://epitmenyado-backend.cyclic.app/",
   withCredentials: true,
 });
 
