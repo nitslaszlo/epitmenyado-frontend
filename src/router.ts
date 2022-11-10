71a31f6c4789c5407dcb477a7260a8adeff9abe3
nitslaszlo <nitslaszlo@gmail.com>
2022-11-10 20:47:56 +0100

    protect router routes

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,7 @@ import EditStreetView from "./views/EditStreetView.vue";
 import NewStreetView from "./views/NewStreetView.vue";
 import EditTaxBandView from "./views/EditTaxBandView.vue";
 import NewTaxBandView from "./views/NewTaxBandView.vue";
+import { useUsersStore } from "./store/usersStore";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -24,36 +25,57 @@ const routes: Array<RouteRecordRaw> = [
     path: "/editStreet",
     name: "editStreet",
     component: EditStreetView,
+    meta: {
+      needsAuth: true,
+    },
   },
   {
     path: "/newStreet",
     name: "newStreet",
     component: NewStreetView,
+    meta: {
+      needsAuth: true,
+    },
   },
   {
     path: "/qtablestreet",
     name: "qtablestreet",
     component: QTableStreetView,
+    meta: {
+      needsAuth: true,
+    },
   },
   {
     path: "/editTaxBand",
     name: "editTaxBand",
     component: EditTaxBandView,
+    meta: {
+      needsAuth: true,
+    },
   },
   {
     path: "/newTaxBand",
     name: "newTaxBand",
     component: NewTaxBandView,
+    meta: {
+      needsAuth: true,
+    },
   },
   {
     path: "/qtabletaxband",
     name: "qtabletaxband",
     component: QTableTaxBandView,
+    meta: {
+      needsAuth: true,
+    },
   },
   {
     path: "/about",
     name: "about",
     component: AboutView,
+    meta: {
+      needsAuth: true,
+    },
   },
 ];
 const router = createRouter({
@@ -62,6 +84,15 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  next();
+  if (to.meta.needsAuth) {
+    const usersStore = useUsersStore();
+    if (usersStore.getLoggedUser) {
+      next();
+    } else {
+      next("/account");
+    }
+  } else {
+    next();
+  }
 });
 export default router;
